13ef6fece60cc639753e996b8e8476ba811de1c0
Guadalupe <mgtfele@gmail.com>
2025-04-02 21:08:00 -0600

    Incorporación del selector de horario en el formulario a través de librería, ajustes de los bloques del home e incorporación de bloques para about us y our collection

--- scripts/actions-owl.js
+++ scripts/actions-owl.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     $('.owl-presentation').owlCarousel({
+        center:true,
         loop:true,
+        autoplay:true,
         nav:true,
-        margin:16,
+        margin:7, //16
         navText: [
             `<div class='arrow'>
                 <svg fill="currentColor" height="256px" width="256px" version="1.1" id="XMLID_287_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" xml:space="preserve" transform="rotate(180)">
@@ -20,29 +22,34 @@ $(document).ready(function(){
             </div>`
         ],
         dots:true,
-        autoplay:true,
+        navContainer: ".customControls", // Mueve prev/next aquí
+        dotsContainer: ".customDots", // Mueve los dots aquí
         responsive:{
             0:{
                 items:1,
-                stagePadding:15,
+                stagePadding:9,//15
             },
             600:{
-                items:3,
+                items:2,
+                margin:16,
                 stagePadding:0,
             },
             900:{
+                items:3,
+                margin:16,
                 stagePadding:0,
             }
             
         }
     });  
+
     
     $('.logos-owl').owlCarousel({
         loop:true,
         nav:false,
         margin:80,
-        dots:true,
-        autoplay:false,
+        dots:false,
+        autoplay:true,
         responsive:{
             0:{
                 items:1
@@ -56,4 +63,58 @@ $(document).ready(function(){
             
         }
     });
+
+    $('.owl-cards').owlCarousel({
+        center:true,
+        loop:true,
+        autoplay:true,
+        nav:true,
+        margin:7,
+        navText: [
+            `<div class='arrow'>
+                <svg fill="currentColor" height="256px" width="256px" version="1.1" id="XMLID_287_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" xml:space="preserve" transform="rotate(180)">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+                    <g id="SVGRepo_iconCarrier"> <g id="next"> <g> <polygon points="6.8,23.7 5.4,22.3 15.7,12 5.4,1.7 6.8,0.3 18.5,12 "/> </g> </g> </g>
+                </svg>
+            </div>`,
+            `<div class='arrow'>
+                <svg fill="currentColor" height="256px" width="256px" version="1.1" id="XMLID_287_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" xml:space="preserve">
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+                    <g id="SVGRepo_iconCarrier"> <g id="next"> <g> <polygon points="6.8,23.7 5.4,22.3 15.7,12 5.4,1.7 6.8,0.3 18.5,12 "/> </g> </g> </g>
+                </svg>
+            </div>`
+        ],
+        dots:true,
+        navContainer: ".customControlsCards", // Mueve prev/next aquí
+        dotsContainer: ".customDotsCards", // Mueve los dots aquí
+        responsive:{
+            0:{
+                items:1,
+                stagePadding:9,
+            },
+            600:{
+                center:false,
+                items:2,
+                margin:16,
+                stagePadding:0,
+            },
+            900:{
+                center:false,
+                items:3,
+                margin:16,
+                stagePadding:0,
+            }
+            ,
+            1200:{
+                center:false,
+                items:4,
+                margin:16,
+                stagePadding:0,
+            }
+            
+        }
+    });  
+    
 });
--- scripts/actions.js
+++ scripts/actions.js
@@ -1,17 +1,31 @@
 $(document).ready(function(){
 
-    /* Agregar clase para el select */
-    $('.select_action').each(function() {
-        var selectElement = this;
-        var selectContainer = $(selectElement).closest('.select_select');
-  
-        $(selectElement).on('focus', function() {
-          selectContainer.addClass('openSelect');
-        });
-  
-        $(selectElement).on('blur', function() {
-          selectContainer.removeClass('openSelect');
-        });
+  /* Agregar clase para el select */
+  $('.select_action').each(function() {
+      var selectElement = this;
+      var selectContainer = $(selectElement).closest('.select_select');
+
+      $(selectElement).on('focus', function() {
+        selectContainer.addClass('openSelect');
       });
-      
+
+      $(selectElement).on('blur', function() {
+        selectContainer.removeClass('openSelect');
+      });
+    });
+
+  /* Galería */
+    $('.lightgallery').lightGallery({
+      mode: 'lg-fade',
+      selector: '.item_gallery_img_lg',
+    });
+  /* Input Time */
+    $("#timepicker").datetimepicker({
+      format: "LT",
+      icons: {
+      up: "fa fa-chevron-up",
+      down: "fa fa-chevron-down"
+      }
+  });
+    
 });
